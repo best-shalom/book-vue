7fe4aeaace7af0f6dc3212e55df28882e6d712e3
CQ <2227558856@qq.com>
2024-05-30 11:11:28 +0800

    feat(导航栏): 抽象出导航栏组件，并优化结构和内容
    1.实现导航栏不同链接指向（其他页面没做）
    2.优化导航栏布局和样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,6 @@
 <template>
+  <Navbar/>
   <div id="app">
-    <!-- 导航栏 position: fixed;将导航栏固定在视口中设置导航栏的堆叠顺序为 1000-->
-    <nav
-        style="background-color: #f8f9fa; position: fixed; width: 100%; top: 0; z-index: 1000; box-shadow: 0 2px 4px rgba(0,0,0,.1);">
-      <!--
-      去除列表项的默认样式，使用弹性布局（Flex布局），列表项将按照水平方向排列，在列表中均匀分布。
-      -->
-      <ul style="list-style: none; padding: 10px 20px; margin: 0; display: flex; justify-content: space-around;">
-        <!--
-        v-for="genre in genres"：这是 Vue.js 中的循环指令，它会遍历 genres 数组中的每个元素，并为每个元素创建一个列表项。
-        :key="genre"：这是 Vue.js 中的 key 绑定，用于帮助 Vue.js 识别列表项的唯一性，以提高渲染性能。通常情况下，我们会将列表项的唯一标识作为 key。
-        当用户点击列表项时，会调用 changeFilter 修改页面内容
-        -->
-        <li v-for="genre in genres" :key="genre" style="padding: 10px;" @click="changeFilter(genre)">
-          <!--
-          href="#"：这是链接的目标 URL，# 表示当前页面的位置。在这种情况下，点击链接不会导航到页面的其他位置，而是停留在当前页面。这种用法通常用于实现具有点击行为但不需要实际导航的元素。
-          {{ genre }}：这是一个插值表达式，它会在页面渲染时被替换为 Vue.js 实例中的 genre 变量的值。这样做的目的是在链接文本中显示 genre 变量的值，使用户能够直观地了解链接所代表的内容。
-          -->
-          <a href="#" style="text-decoration: none; color: #333; font-weight: bold;">{{ genre }}</a>
-        </li>
-      </ul>
-    </nav>
     <!-- 内容下移，避免被导航栏遮挡 -->
     <div style="margin-top: 60px;">
       <!-- 使用书籍列表组件，并传递书籍数据 -->
@@ -32,20 +12,21 @@
       在分页器组件中，当页码发生变化时，通过 $emit 触发 update:currentPage 事件，并传递新的页码值给父组件。
       最后，在父组件中监听分页器组件发出的 update:currentPage 事件，将收到的新页码值更新到父组件的数据中，触发数据更新，从而重新加载对应页的数据。
       -->
-      <Pagination :currentPage="this.pages.pageNum" :totalPages="this.pages.total"
-                  @update:currentPage="updateCurrentPage"
-      />
     </div>
   </div>
+  <Pagination :currentPage="this.pages.pageNum" :totalPages="this.pages.total"
+              @update:currentPage="updateCurrentPage"/>
 </template>
 
 
 <script>
+import Navbar from './Navbar.vue'
 import BookList from './BookList.vue';
 import Pagination from "@/components/Pagination.vue";
 
 export default {
   components: {
+    Navbar,
     // 注册 BookList 组件
     BookList,
     Pagination
---
+++ src/views/Navbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li><a href="#">首页</a></li>
+        <li><a href="/">全部</a></li>
+        <li><a href="/rank">排行</a></li>
+        <li><a href="/tag">标签</a></li>
+        <li><a href="/search">搜索</a></li>
+      </ul>
+    </nav>
+  </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+/* 导航栏本身固定视口，使<nav>元素内部的子元素纵向居中排列*/
+nav {
+  position: fixed;
+  width: 100%;
+  height: 60px;
+  top: 0;
+  padding: 0;
+  box-shadow: 0 5px 7px #666666;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+/* 如果想知道一个元素的所在布局，以及调整时的作用范围，就可以将元素添加背景色，方便观察元素布局*/
+ul {
+  margin: 0;
+  padding: 0; /* <ul> 元素会有一些默认的内外边距*/
+  display: flex;
+  justify-content: space-around; /* 将列表项水平排列 */
+  list-style: none;
+  flex-grow: 1; /* 横向占满nav的剩余空间*/
+  height: 100%; /* 由于 <ul> 元素没有设置具体的高度，且父元素 <nav> 设置了固定的高度为 60px，所以可能导致 <ul> 元素无法完全占满 <nav> 元素的高度。*/
+}
+
+li {
+  display: flex;
+  justify-content: space-around;
+  flex-grow: 1;
+  background-color: #cccccc;
+  border-right: 1px solid #666666; /* solid表示边框实线*/
+}
+
+a {
+  margin: auto;
+  text-decoration: none;
+  color: #333333;
+}
+
+</style>
