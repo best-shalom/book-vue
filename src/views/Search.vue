be887a6d119d06de9519812633cc17cd18bb70fc
CQ <2227558856@qq.com>
2024-07-11 14:12:49 +0800

    feat(书籍搜索): 框架搭建，逻辑梳理

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,17 +1,50 @@
 <template>
   <Navbar/>
-  <h1>
-    高级搜索、多条件搜索、书名作者主角名搜索or模糊搜索、全文模糊搜索
-    也就是根据信息找书
-  </h1>
+  <!--
+  1.过滤条件：
+  书籍分类、标签、阅读类型的过滤条件
+  2.搜索条件：
+  书名: 可以进行完全匹配或模糊搜索。
+  作者: 通过作者姓名进行搜索。
+  主角名: 根据书中主角的名字进行搜索。
+  关键词: 通过关键字或者简介段落进行全文搜索。
+  3.排序：
+  完结时间: 根据书籍的完结时间进行排序或指定范围。
+  评分: 按照书籍的评分进行排序或指定范围。
+  -->
+  <div class="search-book">
+    <div class="search-container">
+      <div class="filter-info">
+
+      </div>
+      <div class="search-info">
+
+      </div>
+      <div class="sort-info">
+
+      </div>
+
+    </div>
+    <div class="book-list__search">
+      <BookList :books="books"/>
+    </div>
+  </div>
 </template>
 
 <script>
 import Navbar from '@/components/home/Navbar.vue'
+import BookList from "@/components/common/BookList.vue";
 
 export default {
   components: {
+    BookList,
     Navbar
+  },
+  data() {
+    return {
+      searchInfo: {},
+      books: []
+    }
   }
 }
 </script>
